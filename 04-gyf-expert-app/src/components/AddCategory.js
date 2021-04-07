e83a31ea1374498468890e5e6d1850c7131924e5
Votaiza <victorotaiza2011@gmail.com>
2021-04-07 11:44:53 +0000

    GifExpertApp Finalizado

--- 04-gyf-expert-app/src/components/AddCategory.js
+++ 04-gyf-expert-app/src/components/AddCategory.js
@@ -15,7 +15,7 @@ export default function AddCategory( { setCategories } ) {
         e.preventDefault();
 
         if (inputValue.trim().length > 2) {
-            setCategories( (cats) => [...cats, inputValue]);
+            setCategories( (cats) => [inputValue, ...cats]);
             setInputValue('');
         }
     }
--- 04-gyf-expert-app/src/components/GifGrid.js
+++ 04-gyf-expert-app/src/components/GifGrid.js
@@ -1,38 +1,20 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-    const [imagen, setImagen] = useState([]);
 
-    useEffect(() => {
-        getGifs();
-    }, []);
-
-    const getGifs = async() => {
-        
-        const url = 'https://api.giphy.com/v1/gifs/search?api_key=cA4zJcSSxIDzIIjM0AjqZqnPVJTHae71&q=comedia&limit=10'
-        const resp = await fetch( url );
-        const {data} = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url //El signo ? pregunta si existe el atributo
-            }
-        } )
-
-        console.log(gifs);
-
-        setImagen(gifs);
-    }
-
+    const {data:imagen, loading} = useFetchGifs( category);
     
 
     return (
         <>
         <h3>{category}</h3>
+
+        { loading && 'Cargando' }
+        
         <div className="card-grid">
             {
                 imagen.map( img => (
@@ -44,6 +26,8 @@ export const GifGrid = ({category}) => {
             }
         
         </div>
+       
         </>
+        
     )
 }
